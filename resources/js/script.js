3aec7430d0b41bf63c5104efa6a27b204e57a904
efeaizesogie <67043916+efeaizesogie@users.noreply.github.com>
2021-07-28 18:23:56 +0100

    third commit

--- resources/js/script.js
+++ resources/js/script.js
@@ -34,4 +34,18 @@ $(document).ready(function() {
           }
         });
       });
+
+      $(".js--wp-1").waypoint(function(){
+        $(".js--wp-1").addClass("animated fadeInUp");
+      },
+      {
+        offset: "50%"
+      });
+
+      $(".js--wp-2").waypoint(function(){
+        $(".js--wp-2").addClass("animated fadeInLeft");
+      },
+      {
+        offset: "50%"
+      });
 });
