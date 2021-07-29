be23a2301d668a3866756a3d27a775bbc1dce868
efeaizesogie <67043916+efeaizesogie@users.noreply.github.com>
2021-07-29 21:14:54 +0100

    forth commit

--- resources/js/script.js
+++ resources/js/script.js
@@ -48,4 +48,18 @@ $(document).ready(function() {
       {
         offset: "50%"
       });
+
+      $(".js--mobile-nav").click(function(){
+        var nav = $(".js--sidebar");
+        var icon = $(".js--mobile-nav i");
+        nav.slideToggle(200)
+        if(icon.hasClass("ion-navicon-round")){
+          icon.addClass("ion-close-round")
+          icon.removeClass("ion-navicon-round")
+        } else{
+          icon.addClass("ion-navicon-round")
+          icon.removeClass("ion-close-round")
+        }
+       });
+
 });
